90ab8acea19a814b651724ac481341b5f9c6b6f3
Akachukwu <udechukwuakachukwu@gmail.com>
2025-01-04 23:02:41 +0100

    Created the CreateUser endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,24 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { User } from './entity/user.entity';
 
 @Controller('users')
 export class UsersController {
     constructor (private usersService:UsersService){}
     @Get()
-    getUsers():any{
+    getUsers():User[]{
         return this.usersService.findAll()
     }
 
     @Get(':id')
-    getUserById(@Param('id') id:string):any {
+    getUserById(@Param('id') id:string):User {
         return this.usersService.findOne(Number(id))
     }
+
+    @Post()
+    createUser(@Body() body:CreateUserDTO):User{
+        return this.usersService.createUser(body)
+    }
+    
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { User } from './entity/user.entity';
 
 @Injectable()
 export class UsersService {
     private users:any = [{id: 1,name:'John',email:'john@gmail.com'}, { id: 2,name:'Jane',email:'jane@gmail.com'}];
 
-    findAll(){
+    findAll():User[]{
         return this.users
     }
 
-    findOne(userID:number){
+    findOne(userID:number): User{
         return this.users.find((user:any)=> user.id === userID)
+    }
+
+    createUser(body:CreateUserDTO):User {
+        const newUser = {
+            id: Date.now(),
+            ...body
+        }
+
+        this.users.push(newUser)
+        return newUser
     }
 }
