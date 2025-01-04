417d4ac077a7eb76733fd0cae36bf9da7ee14ef5
Akachukwu <udechukwuakachukwu@gmail.com>
2025-01-04 22:44:14 +0100

    Created the user endpoints functions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-    
+    constructor (private usersService:UsersService){}
+    @Get()
+    getUsers():any{
+        return this.usersService.findAll()
+    }
+
+    @Get(':id')
+    getUserById(@Param('id') id:string):any {
+        return this.usersService.findOne(Number(id))
+    }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users:any = [{id: 1,name:'John',email:'john@gmail.com'}, { id: 2,name:'Jane',email:'jane@gmail.com'}];
+
+    findAll(){
+        return this.users
+    }
+
+    findOne(userID:number){
+        return this.users.find((user:any)=> user.id === userID)
+    }
+}
